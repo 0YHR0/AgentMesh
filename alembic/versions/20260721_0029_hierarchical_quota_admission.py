cdd96395b00b00208627686374653301a480dceb
0YHR0 <97868579@qq.com>
2026-07-21 16:41:06 +0800

    Fix migration import ordering

--- alembic/versions/20260721_0029_hierarchical_quota_admission.py
+++ alembic/versions/20260721_0029_hierarchical_quota_admission.py
@@ -4,9 +4,9 @@ Revision ID: 20260721_0029
 Revises: 20260721_0028
 """
 
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 revision = "20260721_0029"
 down_revision = "20260721_0028"
